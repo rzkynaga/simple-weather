3995784a8a6fc2e0c47fed744992eb12f29fa832
Mohammad Rizky Sinaga <90925683+rzkynaga@users.noreply.github.com>
2024-12-18 10:52:17 +0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
   plugins: [],
 };
